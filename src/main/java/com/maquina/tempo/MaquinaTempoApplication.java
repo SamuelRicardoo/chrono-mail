0288361def6fab5d377df6de7687cbd9a9e38c51
SamuelRicardoo <sricardosgomes@hotmail.com>
2025-08-01 21:05:32 -0300

    first commit

---
+++ src/main/java/com/maquina/tempo/MaquinaTempoApplication.java
@@ -0,0 +1,13 @@
+package com.maquina.tempo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaquinaTempoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaquinaTempoApplication.class, args);
+    }
+
+}
