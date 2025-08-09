5b5056c7bae47917400da721ff9ab50de83c22e3
SamuelRicardoo <sricardosgomes@hotmail.com>
2025-08-09 14:28:48 -0300

    feat: implementation schedulingRepository

--- src/main/java/com/maquina/tempo/repository/SchedulingRepository.java
+++ src/main/java/com/maquina/tempo/repository/SchedulingRepository.java
@@ -2,8 +2,14 @@ package com.maquina.tempo.repository;
 
 import com.maquina.tempo.entities.Scheduling;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SchedulingRepository extends JpaRepository<Scheduling,Long> {
+
+    @Query("SELECT s FROM Scheduling s WHERE DATE(s.date) = CURRENT_DATE")
+    List<Scheduling> findBySchedulingTimeToday();
 }
