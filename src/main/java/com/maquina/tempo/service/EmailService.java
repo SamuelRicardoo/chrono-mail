547d5a9c4c198143cb833c3e5f4d9943b41b51f0
SamuelRicardoo <sricardosgomes@hotmail.com>
2025-08-09 14:27:44 -0300

    feat: implemented sendEmailScheduling in EmailService

--- src/main/java/com/maquina/tempo/service/EmailService.java
+++ src/main/java/com/maquina/tempo/service/EmailService.java
@@ -3,8 +3,6 @@ package com.maquina.tempo.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -37,4 +35,21 @@ public class EmailService {
             e.printStackTrace();
         }
     }
+
+    public void sendEmailScheduling(String to, String subject, String body) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            javaMailSender.send(mimeMessage);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
