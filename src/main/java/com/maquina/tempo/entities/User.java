f2515de5d9a9d5053813b999535cbb34a938119f
SamuelRicardoo <sricardosgomes@hotmail.com>
2025-08-09 14:25:23 -0300

    feat: adjustment User

--- src/main/java/com/maquina/tempo/entities/User.java
+++ src/main/java/com/maquina/tempo/entities/User.java
@@ -1,7 +1,5 @@
 package com.maquina.tempo.entities;
 
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
